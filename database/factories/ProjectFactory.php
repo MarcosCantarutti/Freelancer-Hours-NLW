aa5c7762e63ec82e392234a3e36ebb7ffce78c30
rodrigues <marcos.rodrigues@sh3.com.br>
2024-10-10 13:38:24 -0300

    aula 02

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -23,7 +23,7 @@ class ProjectFactory extends Factory
             'description' => fake()->randomHtml(),
             'ends_at' => fake()->dateTimeBetween('now', '+ 3 days'),
             'status' => fake()->randomElement(['open', 'closed']),
-            'tech_stack' => fake()->randomElements(['react', 'php', 'laravel', 'vue', 'tailwind', 'nextjs', 'python'], random_int(1, 5)),
+            'tech_stack' => fake()->randomElements(['react', 'nodejs', 'vite', 'javascript', 'nextjs'], random_int(1, 5)),
             'created_by' => User::factory(),
         ];
     }
